98b43e1464c18ca96c61b40b9a5b035996dbbaf5
Victor1514-soft <victor.dejesus10@upr.edu>
2026-05-11 13:34:22 -0400

    removing build files

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calculator_FILE C:/Users/gamer/Downloads/Calculator/build/Calculator.exe)
-set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calculator_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
